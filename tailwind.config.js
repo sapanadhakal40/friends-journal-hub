41cb39dacef426593cb99bfd7896c86af2921430
sapanadhakal40 <sapna.dhakal40@gmail.com>
2024-11-11 09:56:46 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html', // Adjust this path based on where your HTML files are located
+    './static/js/**/*.js'     // If you have JavaScript files in a static folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
