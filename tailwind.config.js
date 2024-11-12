d0ebe18e11d58d1ca85dc31bf086a831a0563a3f
sapanadhakal40 <sapna.dhakal40@gmail.com>
2024-11-13 00:26:17 +0200

    update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html', // Adjust this path based on where your HTML files are located
-    './static/js/**/*.js'     // If you have JavaScript files in a static folder
+    './app/templates/**/*.html', // Adjust this path based on where your HTML files are located
+    './app/static/js/**/*.js'     // If you have JavaScript files in a static folder
   ],
   theme: {
     extend: {},
